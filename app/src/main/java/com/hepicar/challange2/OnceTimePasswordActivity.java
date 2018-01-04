9c1ef23cb2867f7009efe20353216220f0d97fdc
omayib <omayib@gmail.com>
2018-01-04 10:31:15 +0700

    initial apps for second challenge

---
+++ app/src/main/java/com/hepicar/challange2/OnceTimePasswordActivity.java
@@ -0,0 +1,13 @@
+package com.hepicar.challange2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OnceTimePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_once_time_password);
+    }
+}
